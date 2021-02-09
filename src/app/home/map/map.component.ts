a6baca10f6fc82b0b47823c09edfac6d42495833
estebanJones <teixeirac.jordan@gmail.com>
2021-02-09 12:46:41 +0100

    test map

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,15 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsMapTypeId,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker,
-  Environment
-} from '@ionic-native/google-maps';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {GoogleMaps, GoogleMap,GoogleMapsEvent,LatLng} from '@ionic-native/google-maps';
 import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
 
 @Component({
@@ -17,34 +7,37 @@ import { ActionSheetController, Platform, AlertController } from '@ionic/angular
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
   map: GoogleMap;
 
   constructor( public alertController: AlertController, public actionCtrl: ActionSheetController, private platform: Platform ) { 
-    if (this.platform.is('cordova')) {
-      this.loadMap();
-    }
     
   }
 
   ngOnInit() {}
 
   loadMap() {
-    Environment.setEnv({
-      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyCH3ILcHPLgysmK8T65TPrEkbgPtfDNnXg',
-      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyCH3ILcHPLgysmK8T65TPrEkbgPtfDNnXg'
-    });
-    this.map = GoogleMaps.create('map_canvas', {
-      camera: {
-        target: {
-          lat: 43.6600980666535,
-          lng: 3.035913988993468
-        },
-        zoom: 12,
-        tilt: 30
-      }
-    });
+    const map = GoogleMaps.create('map_canvas');
+    map.one( GoogleMapsEvent.MAP_READY ).then((data: any) => {
+			const coordinates: LatLng = new LatLng(43.6600980666535, 3.035913988993468);
+
+			map.setCameraTarget(coordinates);
+			map.setCameraZoom(12);
+		});
+    // this.map = GoogleMaps.create('map_canvas', {
+    //   camera: {
+    //     target: {
+    //       lat: 43.6600980666535,
+    //       lng: 3.035913988993468
+    //     },
+    //     zoom: 12,
+    //     tilt: 30
+    //   }
+    // });
   }
 
+  ngAfterViewInit() {
+		this.platform.ready().then(() => this.loadMap());
+	}
 }
