fb34a95bcf7f7ddcc1238a857bef6a1d160a2de9
Jefferon AFANOU <jefferson.afanou@gmail.com>
2021-02-09 10:18:28 +0100

    Structure in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home/map',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'home/map',
+    loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
+  },
+  /* {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'favoris',
+    loadChildren: () => import('./favorites/favorites.module').then( m => m.FavoritesPageModule)
+  },
+  {
+    path: 'preference',
+    loadChildren: () => import('./preferences/preference.module').then( m => m.PreferencePageModule)
+  } */
 ];
 
 @NgModule({
